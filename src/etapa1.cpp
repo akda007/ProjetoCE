6e46cc915c734aca631b86db46b41887aa1c2f81
akda007 <akdagmr007s@gmail.com>
2026-04-15 21:53:00 -0300

    Finished the steps 2 and 3

--- src/etapa1.cpp
+++ src/etapa1.cpp
@@ -1,49 +1,49 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-static uint32_t RED_PIN = 25;
-static uint32_t YELLOW_PIN = 33;
-static uint32_t GREEN_PIN = 32;
+// static uint32_t RED_PIN = 25;
+// static uint32_t YELLOW_PIN = 33;
+// static uint32_t GREEN_PIN = 32;
 
-enum Color {
-  RED,
-  YELLOW,
-  GREEN
-};
+// enum Color {
+//   RED,
+//   YELLOW,
+//   GREEN
+// };
 
-Color current_color = RED;
+// Color current_color = RED;
 
-uint32_t colors[] = {RED_PIN, YELLOW_PIN, GREEN_PIN};
+// uint32_t colors[] = {RED_PIN, YELLOW_PIN, GREEN_PIN};
 
-void setup() {
-  Serial.begin(115200);
+// void setup() {
+//   Serial.begin(115200);
 
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(YELLOW_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-}
+//   pinMode(RED_PIN, OUTPUT);
+//   pinMode(YELLOW_PIN, OUTPUT);
+//   pinMode(GREEN_PIN, OUTPUT);
+// }
 
-void turn_leds_off( ){
-  digitalWrite(RED_PIN, LOW);
-  digitalWrite(YELLOW_PIN, LOW);
-  digitalWrite(GREEN_PIN, LOW);
-}
+// void turn_leds_off( ){
+//   digitalWrite(RED_PIN, LOW);
+//   digitalWrite(YELLOW_PIN, LOW);
+//   digitalWrite(GREEN_PIN, LOW);
+// }
 
-String get_color_name(Color color) {
-  return color == RED ? "Vermelha" : color == YELLOW ? "Amarela" : "Verde";
-}
+// String get_color_name(Color color) {
+//   return color == RED ? "Vermelha" : color == YELLOW ? "Amarela" : "Verde";
+// }
 
-void update_color() {
-  current_color = static_cast<Color>((((int)current_color + 1) % 3));
+// void update_color() {
+//   current_color = static_cast<Color>((((int)current_color + 1) % 3));
 
-  turn_leds_off();
+//   turn_leds_off();
 
-  digitalWrite(colors[current_color], HIGH);
+//   digitalWrite(colors[current_color], HIGH);
 
-  Serial.printf("Ligando a luz %s\n", get_color_name(current_color));
-}
+//   Serial.printf("Ligando a luz %s\n", get_color_name(current_color));
+// }
 
-void loop() {
-  sleep(1);
-  update_color();
-}
+// void loop() {
+//   sleep(1);
+//   update_color();
+// }
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,22 @@ static uint32_t RED_PIN = 25;
 static uint32_t YELLOW_PIN = 33;
 static uint32_t GREEN_PIN = 32;
 
+static uint32_t POT_PIN = 35;
+static uint32_t BUTTON_PIN = 13;
+
+bool system_paused = false;
+bool  night_mode = false;
+
+uint64_t semaphore_delay = 0;
+
+int last_button_pressed = 0;
+uint64_t button_start_press = 0;
+
+uint64_t nightmode_delay = 0;
+bool night_mode_state = false;
+
+uint64_t ciclos = 0, changes = 0;
+
 enum Color {
   RED,
   YELLOW,
@@ -20,6 +36,9 @@ void setup() {
   pinMode(RED_PIN, OUTPUT);
   pinMode(YELLOW_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
+
+  pinMode(POT_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
 }
 
 void turn_leds_off( ){
@@ -39,11 +58,74 @@ void update_color() {
 
   digitalWrite(colors[current_color], HIGH);
 
-  Serial.printf("Ligando a luz %s\n", get_color_name(current_color));
+  Serial.printf("Mudando para a luz %s\n", get_color_name(current_color));
 }
 
+
+
 void loop() {
-  sleep(1);
-  update_color();
+  int pot_input = analogRead(POT_PIN);
+  int led_delay = 200 + pot_input * 1800 / 4095;
+
+  int button_pressed = digitalRead(BUTTON_PIN);
+
+  if (last_button_pressed == true && !button_pressed) {
+
+    if (millis() - button_start_press > 2000) {
+      night_mode = !night_mode;
+
+      if (night_mode) {
+        Serial.println("Nightmode Ligado!");
+      } else {
+        Serial.println("Nightmode Desligado!");
+      }
+    } else {
+      if (system_paused) {
+        Serial.println("Sistema Despausado!!!");
+      } else {
+        Serial.println("Sistema Pausado!!!");
+      }
+
+      system_paused = !system_paused;
+    }
+    
+  }
+
+  if (!last_button_pressed && button_pressed) {
+    delay(30);
+    button_start_press = millis();
+  }
+
+  last_button_pressed = button_pressed;
+
+
+  if (!night_mode && !system_paused && millis() - semaphore_delay > led_delay) {
+    semaphore_delay = millis();
+
+    update_color();
+
+    changes++;
+
+    if (changes % 3 == 0) {
+      ciclos++;
+    }
+
+    if (changes % 15 == 0) {
+      Serial.printf("%i ciclos completos!\n", ciclos);
+    }
+  }
+
+  if (night_mode && millis() - nightmode_delay > 500) {
+    turn_leds_off();
+
+    digitalWrite(YELLOW_PIN, night_mode_state);
+    night_mode_state = !night_mode_state;
+
+    nightmode_delay = millis();
+
+    
+  }
+
+  // Serial.printf("Potenciometro: %i, delay = %i\n", pot_input, led_delay);
 }
 
